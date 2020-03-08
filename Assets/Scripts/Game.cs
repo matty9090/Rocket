a02a723dae1e20b1a22146abec7ddf5721d07f6d
Matthew Lowe <lowe.matthew@mail.com>
2020-03-08 14:02:10 +0000

    Add aiming and power bar

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -11,15 +11,29 @@ public class Game : MonoBehaviour
     private Transform Rocket = null;
 
     [SerializeField]
+    private PowerBar PowerBar = null;
+
+    [SerializeField]
     private Transform LaunchOrigin = null;
 
     [SerializeField]
     private AnimationCurve LaunchCurve = null;
 
+    [SerializeField]
+    private AnimationCurve PowerCurve = null;
+
+    [SerializeField]
+    private float AimSpeed = 0.5f;
+
+    [SerializeField]
+    private float PowerSpeed = 0.5f;
+
     private enum EState { Aim, Fly, Land };
     private EState CurrentState = EState.Aim;
 
     // Aim state
+    private enum EAimSubState { Angle, Power };
+    private EAimSubState AimState = EAimSubState.Angle;
     private float AimTimer = 0.0f;
 
     void Start()
@@ -31,27 +45,52 @@ public class Game : MonoBehaviour
     {
         switch (CurrentState)
         {
-            case EState.Aim: AimState(); break;
-            case EState.Fly: FlyState(); break;
-            case EState.Land: LandState(); break;
+            case EState.Aim: TickAimState(); break;
+            case EState.Fly: TickFlyState(); break;
+            case EState.Land: TickLandState(); break;
         }
     }
 
-    void AimState()
+    void TickAimState()
     {
-        AimTimer += Time.deltaTime * 0.5f;
-        float rot = LaunchCurve.Evaluate(AimTimer) * 0.4f;
-        Debug.Log(rot);
+        if (AimState == EAimSubState.Angle)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                AimTimer = 0.0f;
+                AimState = EAimSubState.Power;
+                Rocket.GetComponentInChildren<TrailRenderer>().enabled = false;
+                return;
+            }
 
-        Rocket.RotateAround(LaunchOrigin.position, new Vector3(0.0f, 0.0f, 1.0f), rot);
+            AimTimer += Time.deltaTime * AimSpeed;
+            float rot = LaunchCurve.Evaluate(AimTimer) * AimSpeed / 0.9f;
+
+            Rocket.RotateAround(LaunchOrigin.position, new Vector3(0.0f, 0.0f, 1.0f), rot);
+        }
+        else if(AimState == EAimSubState.Power)
+        {
+            AimTimer += Time.deltaTime * PowerSpeed;
+            float power = PowerCurve.Evaluate(AimTimer);
+            PowerBar.transform.localScale = new Vector3(1.0f, power, 1.0f);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                AimTimer = 0.0f;
+                AimState = EAimSubState.Angle;
+                CurrentState = EState.Fly;
+                Rocket.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                return;
+            }
+        }
     }
 
-    void FlyState()
+    void TickFlyState()
     {
         Cam.transform.position += Vector3.right * Time.deltaTime * 2.0f;
     }
 
-    void LandState()
+    void TickLandState()
     {
         
     }
---
+++ Assets/Scripts/PowerBar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBar : MonoBehaviour
+{
+    [SerializeField]
+    private Gradient Colour = null;
+
+    void Update()
+    {
+        GetComponent<Image>().color = Colour.Evaluate(transform.localScale.y);
+    }
+}
